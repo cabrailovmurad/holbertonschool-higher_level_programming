50a8dff322682494b72b22320d5fff518e6acbd8
cabrailovmurad <dmurad908@gmail.com>
2025-09-06 21:28:25 +0000

    vsevmesphy

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str1 = "object"
-str2 = "-oriented programming with Python"
-print(str1 + str2)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[0:6]
+print(str)
